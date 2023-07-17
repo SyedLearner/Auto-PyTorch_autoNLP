9769df70ab270924d7b919fc304cb96595878de6
ParisaSafikhani <107673563+ParisaSafikhani@users.noreply.github.com>
2023-07-17 15:06:27 +0200

    Update base_encoder.py

--- autoPyTorch/pipeline/components/preprocessing/tabular_preprocessing/encoding/base_encoder.py
+++ autoPyTorch/pipeline/components/preprocessing/tabular_preprocessing/encoding/base_encoder.py
@@ -1,21 +1,26 @@
+# Import necessary modules and classes
 from typing import Any, Dict, List
-
-from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.base_tabular_preprocessing import (
-    autoPyTorchTabularPreprocessingComponent
-)
+from autoPyTorch.pipeline.components.preprocessing.tabular_preprocessing.base_tabular_preprocessing import autoPyTorchTabularPreprocessingComponent
 from autoPyTorch.utils.common import FitRequirement
 
-
+# Define a class named BaseEncoder that extends the autoPyTorchTabularPreprocessingComponent class
 class BaseEncoder(autoPyTorchTabularPreprocessingComponent):
     """
     Base class for encoder
     """
+
+    # Constructor method
     def __init__(self) -> None:
+        # Call the constructor of the parent class
         super().__init__()
+
+        # Define fit requirements for the encoder
         self.add_fit_requirements([
             FitRequirement('categorical_columns', (List,), user_defined=True, dataset_property=True),
-            FitRequirement('categories', (List,), user_defined=True, dataset_property=True)])
+            FitRequirement('categories', (List,), user_defined=True, dataset_property=True)
+        ])
 
+    # Method to transform the input dictionary
     def transform(self, X: Dict[str, Any]) -> Dict[str, Any]:
         """
         Adds the self into the 'X' dictionary and returns it.
@@ -25,8 +30,14 @@ class BaseEncoder(autoPyTorchTabularPreprocessingComponent):
         Returns:
             (Dict[str, Any]): the updated 'X' dictionary
         """
+
+        # Check if the encoder has been fitted by verifying if the 'numerical' and 'categorical' attributes are None
         if self.preprocessor['numerical'] is None and self.preprocessor['categorical'] is None:
-            raise ValueError("cant call transform on {} without fitting first."
-                             .format(self.__class__.__name__))
+            # Raise a ValueError if the encoder has not been fitted
+            raise ValueError(f"Cannot call transform on {self.__class__.__name__} without fitting first.")
+
+        # Update the 'X' dictionary by adding the encoder to it
         X.update({'encoder': self.preprocessor})
+        
+        # Return the updated 'X' dictionary
         return X
